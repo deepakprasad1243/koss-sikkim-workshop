c9186a3641aece34dcf7ee689113139dd54162fe
deepak <b160026@nitsikkim.ac.in>
2017-10-08 15:05:26 +0530

    hello world java code corrected class name changed from koss to hello_world

--- hello_world.java
+++ hello_world.java
@@ -1,8 +1,8 @@
-class koss
+class hello_world
 {
-  public static void main(String args[])
+  public static void main(String[] args)
   {
-    System.out.print("Hello, world!");
+    System.out.println("Hello, world!");
   }
 }
 
